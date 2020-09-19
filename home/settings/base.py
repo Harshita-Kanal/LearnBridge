1e69423673e1ccd08819ad11c8fe8f10cce08e9b
Harshita-Kanal <harshita.kgv@gmail.com>
2020-09-19 18:38:57 +0530

    Login setup

--- home/settings/base.py
+++ home/settings/base.py
@@ -1,6 +1,7 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
 DEBUG = True
 ALLOWED_HOSTS = []
@@ -12,7 +13,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'rest_auth.registration',
     'rest_framework',
     'rest_framework.authtoken',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -75,6 +76,9 @@ REST_FRAMEWORK = {
     ),
 }
 
+
+ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_EMAIL_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'username'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+AUTH_USER_MODEL = "users.User"  # LOOK INSIDE USERS.MODEL
